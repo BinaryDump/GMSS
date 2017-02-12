df59cbd748e719c7d80ff25d4c23e60b24311216
Ignazio Noto <igna.noto@gmail.com>
2017-02-12 10:38:10 +0100

    Remove useless using

--- GiveMeSomeSheets/MainWindowViewModel.cs
+++ GiveMeSomeSheets/MainWindowViewModel.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Drawing.Printing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Input;
 
 namespace GiveMeSomeSheets
